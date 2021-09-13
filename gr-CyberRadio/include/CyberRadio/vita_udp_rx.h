7b048a14f76b140f5cd3edd7d8dfa533e1f892a7
Jim Melton <jwmelto@users.sourceforge.net>
2021-09-13 16:10:09 -0600

    refactor based on gnuradio dev guidance
    According to willcode4, sources and sinks should inherit from gr::block
    Also, the work() function (or general_work() in this case) is expected
    to block until the requested number of items are available.
    
    This implementation sets an output_multiple so that it is only called
    when a whole packet can be consumed. For formats that support it,
    dropped packet detection is implemented by inserted null samples for
    each missing packet.

--- gr-CyberRadio/include/CyberRadio/vita_udp_rx.h
+++ gr-CyberRadio/include/CyberRadio/vita_udp_rx.h
@@ -1,17 +1,17 @@
-/* -*- c++ -*- */
-/* 
+// -*- c++ -*-
+/*
  * Copyright 2017 <+YOU OR YOUR COMPANY+>.
- * 
+ *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3, or (at your option)
  * any later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this software; see the file COPYING.  If not, write to
  * the Free Software Foundation, Inc., 51 Franklin Street,
@@ -22,50 +22,60 @@
 #ifndef INCLUDED_CYBERRADIO_VITA_UDP_RX_H
 #define INCLUDED_CYBERRADIO_VITA_UDP_RX_H
 
+#include <gnuradio/block.h>
 #include <CyberRadio/api.h>
-#include <gnuradio/sync_interpolator.h>
-#include <gnuradio/sync_block.h>
 
 namespace gr {
-  namespace CyberRadio {
+namespace CyberRadio {
 
-    /*!
-     * \brief <+description of block+>
-     * \ingroup CyberRadio
-     *
-     */
-    class CYBERRADIO_API vita_udp_rx : virtual public gr::sync_interpolator
-    {
-     public:
-      typedef boost::shared_ptr<vita_udp_rx> sptr;
+/*!
+ * \brief <+description of block+>
+ * \ingroup CyberRadio
+ *
+ */
 
-      /*!
-       * \brief Return a shared_ptr to a new instance of vita_udp_rx::vita_udp_rx.
-       *
-       * To avoid accidental use of raw pointers, vita_udp_rx::vita_udp_rx's
-       * constructor is in a private implementation
-       * class. vita_udp_rx::vita_udp_rx::make is the public interface for
-       * creating new instances.
-       */
-      static sptr make(const std::string &src_ip,
-                        unsigned short port,
-                        unsigned int header_byte_offset,
-                        int samples_per_packet,
-                        int bytes_per_packet,
-                        bool swap_bytes,
-                        bool swap_iq,
-                        bool tag_packets,
-                        bool vector_output,
-                        bool uses_v49_1 = true,
-                        bool narrowband = false,
-                        bool debug = false);
-    
-      virtual bool start() = 0;
-      virtual bool stop() = 0;
+// Note: the recommendation from GNU Radio developers is the sources and sinks should
+// always inherit from gr::block, not one of the more derived types
+class CYBERRADIO_API vita_udp_rx : virtual public gr::block
+{
+public:
+    using sptr = boost::shared_ptr<vita_udp_rx>;
 
+    // Because this class takes a number of optional configuration items, and because the
+    // signature changed in a way that could lead to undiagnosed errors, collapse all the
+    // configuration items to a structure and pass that
+    struct Cfg {
+        std::string src_ip;          ///< source IP address to bind to
+        short port;                  ///< source port to bind to
+        unsigned header_byte_offset; ///< number of bytes in the V49 header
+        int samples_per_packet;      ///< number of samples in a packet
+        int bytes_per_packet;        ///< total size of the V49 packet
+        bool swap_bytes;             ///< if the packet should be byteswapped
+        bool swap_iq;                ///< change from IQ to QI (or from QI to IQ)
+        bool tag_packets;            ///< add GR tags to the stream
+        bool uses_v49_1 = true;      ///< VITA 49.1 (VRLP and VEND headers)
+        bool narrowband = false;     ///< if using a narrowband DDC
+        bool debug = false;          ///< output extra debug info
     };
 
-  } // namespace CyberRadio
+    /*!
+     * \brief Return a shared_ptr to a new instance of vita_udp_rx::vita_udp_rx.
+     *
+     * To avoid accidental use of raw pointers, vita_udp_rx::vita_udp_rx's
+     * constructor is in a private implementation class. vita_udp_rx::vita_udp_rx::make
+     * is the public interface for creating new instances.
+     */
+    static auto make(Cfg const& cfg) -> sptr;
+
+    // these are already virtual ... do we need the pure virtual?
+    bool start() override = 0;
+    bool stop() override = 0;
+
+protected:
+    using gr::block::block; // "inherit" base class ctor
+};
+
+} // namespace CyberRadio
 } // namespace gr
 
-#endif /* INCLUDED_CYBERRADIO_VITA_UDP_RX_H */
+#endif // INCLUDED_CYBERRADIO_VITA_UDP_RX_H
--- gr-CyberRadio/lib/vita_udp_rx_impl.h
+++ gr-CyberRadio/lib/vita_udp_rx_impl.h
@@ -1,4 +1,4 @@
-/* -*- c++ -*- */
+// -*- c++ -*-
 /*
  * Copyright 2017 <+YOU OR YOUR COMPANY+>.
  *
@@ -21,152 +21,67 @@
 #ifndef INCLUDED_CYBERRADIO_VITA_UDP_RX_IMPL_H
 #define INCLUDED_CYBERRADIO_VITA_UDP_RX_IMPL_H
 
-// C Includes
-#include <arpa/inet.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <inttypes.h>
-#include <linux/if_ether.h>
-#include <linux/if_packet.h>
-#include <net/ethernet.h>
-#include <net/if.h>
-#include <netdb.h>
-#include <netinet/in.h>
-#include <netinet/ip.h>
-#include <netinet/udp.h>
-#include <poll.h>
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/mman.h>
-#include <sys/socket.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <sys/un.h>
-#include <unistd.h>
-
-#include <CyberRadio/vita_udp_rx.h>
-#include <volk/volk.h>
-
-#define RECV_TIMEOUT 100
-
-std::map<int, float> ndr358_551_ddc_map = {
-  { 0, 0.25e3},
-  { 1, 0.5e3},
-  { 2, 1.0e3},
-  { 3, 2.0e3},
-  { 4, 4.0e3},
-  { 5, 8.0e3},
-  { 6, 16e3},
-  { 7, 32e3},
-  { 8, 64e3},
-  { 9, 128e3},
-  {10, 200e3},
-  {11, 256e3},
-  {12, 400e3},
-  {13, 1.28e6},
-  {14, 3.2e6},
-  {15, 4e6},
-  {16, 2e6},
-  {32, 8e6},
-  {33, 8e6},
-  {34, 16e6},
-  {35, 16e6},
-  {36, 16e6},
-  {37, 32e6},
-  {38, 32e6},
-  {39, 64e6},
-  {40, 128e6}
-};
-
-// V49 Struct Info
-typedef struct {
-  char p;
-  char l;
-  char r;
-  char v;
-  unsigned int frame_info;
-  unsigned int packet_info;
-  unsigned int stream_id;
-  unsigned int class_id_1;
-  unsigned int class_id_2;
-  unsigned int int_timestamp;
-  unsigned int frac_timestamp_msw;
-  unsigned int frac_timestamp_lsw;
-} V49_308_Header;
-
-// VITA 49 VRT IF data packet header, no class ID
-// (used without external framing on NDR354/364)
-typedef struct {
-  unsigned int packet_info;
-  unsigned int stream_id;
-  unsigned int int_timestamp;
-  unsigned int frac_timestamp_msw;
-  unsigned int frac_timestamp_lsw;
-} V49_No491_Header;
-
-typedef struct {
-  uint32_t packet_info;
-  uint32_t stream_id;
-  uint32_t class_0;
-  uint32_t class_1;
-  uint32_t int_timestamp;
-  uint32_t frac_timestamp_msw;
-  uint32_t frac_timestamp_lsw;
-  uint32_t ddc_0;
-  uint32_t ddc_1;
-  uint32_t ddc_2;
-  uint32_t ddc_3;
-  uint32_t ddc_4;
-} V49_0_Header;
+#include "CyberRadio/vita_udp_rx.h"
+#include <vector>
 
 namespace gr {
 namespace CyberRadio {
 
-class vita_udp_rx_impl : public vita_udp_rx {
-private:
-  // Variables
-  int samples_per_packet;
-  bool swap_bytes, swap_iq;
-  bool vector_output;
-  std::string src_ip;
-  unsigned short port;
-  unsigned int header_byte_offset;
-  unsigned int bytes_per_packet;
-  bool uses_v491, is_narrowband;
-  int sock_fd;
-  bool tag_packets;
-  bool debug;
-  uint8_t *buffer;
-  struct pollfd pfd;
-  int d_packetCounter;
-  bool d_coherent;
+class vita_udp_rx_impl : public vita_udp_rx
+{
+    using Base = vita_udp_rx;
 
-  // Methods
-  void tag_packet(int index);
-  int initSocket();
+private:
+    // Variables
+    std::string const d_src_ip;
+    unsigned short const d_port;
+    int d_sock;
+
+    int const d_samples_per_packet;
+    size_t const d_header_byte_offset;
+    size_t const d_bytes_per_packet;
+    bool const d_swap_bytes;
+    bool const d_swap_iq;
+    bool const d_uses_v49_1;
+    bool const d_is_narrowband;
+    bool const d_tag_packets;
+    bool d_debug;
+    bool d_first_packet;
+    unsigned d_packetCounter : 4;
+
+    std::vector<uint8_t> d_buffer;
+
+protected:
+    // Methods
+    auto receive_packet() -> bool;
+    auto process_packet(gr_complex*& outP, int samples_needed) -> int;
+    auto process_v491_packet(gr_complex*& outP) -> int;
+    auto handle_dropped_packet(unsigned packet_counter,
+                               gr_complex*& outP,
+                               int samples_needed) -> int;
+    auto process_IQ(gr_complex*& outP) -> int;
+
+
+    auto tag_packet(int stream, int offset) -> void;
+    auto tag_v491_packet(int stream, int offset) -> void;
 
 public:
-  vita_udp_rx_impl(const std::string &src_ip, unsigned short port,
-                   unsigned int header_byte_offset, int samples_per_packet,
-                   int bytes_per_packet, bool swap_bytes, bool swap_iq,
-                   bool tag_packets, bool vector_output, bool uses_v491 = true,
-                   bool narrowband = false, bool debug = false);
-  ~vita_udp_rx_impl();
+    vita_udp_rx_impl(Cfg const& cfg);
 
-  void rxControlMsg(pmt::pmt_t msg);
-  void txStatusMsg(void);
+    void rxControlMsg(pmt::pmt_t msg);
+    void txStatusMsg();
 
-  bool start();
-  bool stop();
+    bool start() override;
+    bool stop() override;
 
-  // Where all the action really happens
-  int work(int noutput_items, gr_vector_const_void_star &input_items,
-           gr_vector_void_star &output_items);
+    // Where all the action really happens
+    int general_work(int noutput_items,
+                     gr_vector_int& ninput_items,
+                     gr_vector_const_void_star& input_items,
+                     gr_vector_void_star& output_items) override;
 };
 
 } // namespace CyberRadio
 } // namespace gr
 
-#endif /* INCLUDED_CYBERRADIO_VITA_UDP_RX_IMPL_H */
+#endif // INCLUDED_CYBERRADIO_VITA_UDP_RX_IMPL_H
